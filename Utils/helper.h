79f7380ea44a5d86026c255a601a87ef9bebe45a
i-xiaohu <631751599@qq.com>
2023-04-19 15:11:46 +0800

    helper.h: restore mismatch offset.

--- Utils/helper.h
+++ Utils/helper.h
@@ -15,6 +15,24 @@ void read_value(std::istream &in, t_val &value, bool plain_text_read_mode) {
 	else in.read((char*) &value, sizeof(t_val));
 }
 
+template<typename uint_read_len>
+void convert_mis_rev_offset(uint_read_len *offsets, uint8_t mis_cnt, uint_read_len read_length) {
+	// For example, three mismatches encoded as 0 0 2, and the read length is 101.
+	// the real mismatched positions are 96, 99, 100
+	// 101-(0+1) = 100
+	// 100-(0+1) = 99
+	// 99-(2+1)  = 96
+	// This encoding strategy consider mismatches toward 5'end
+	for (int i = 0; i < mis_cnt / 2; i++) {
+		std::swap(offsets[i], offsets[mis_cnt - 1 - i]);
+	}
+	uint_read_len pos = read_length;
+	for (int i = (int)mis_cnt - 1; i >= 0; i--) {
+		pos -= offsets[i] + 1;
+		offsets[i] = pos;
+	}
+}
+
 void read_array(std::istream &in, void *dest_array, size_t array_size_in_bytes);
 
 #endif //PGRC_LEARN_HELPER_H
