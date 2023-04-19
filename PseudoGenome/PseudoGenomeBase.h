1321924ee8c35dbc2d0a0f22c3a8ea7f4cd19eb7
i-xiaohu <631751599@qq.com>
2023-04-19 15:36:38 +0800

    PseudoGenomeBase.cpp deleted.

--- PseudoGenome/PseudoGenomeBase.h
+++ PseudoGenome/PseudoGenomeBase.h
@@ -6,20 +6,37 @@
 #define PGRC_LEARN_PSEUDOGENOMEBASE_H
 
 #include <fstream>
+#include <cassert>
 
 class PseudoGenomeHeader {
 private:
-	const std::string PG_HEADER = "PGEN";
+	std::string PG_HEADER = "PGEN";
 
 	std::string type;
 	bool constant_read_length = true;
-	uint16_t max_read_length;
-	uint32_t reads_count;
-	uint64_t pg_length;
+	int max_read_length = 0;
+	int reads_count = 0;
+	long pg_length = 0;
 
 
 public:
-	PseudoGenomeHeader(std::ifstream &in);
+	PseudoGenomeHeader() = default;
+
+	explicit PseudoGenomeHeader(std::ifstream &in) {
+		std::string header; in >> header; assert(header == PG_HEADER);
+		in >> type;
+		in >> constant_read_length;
+		in >> max_read_length;
+		in >> reads_count;
+		in >> pg_length;
+		in.get();
+	}
+
+	int get_max_read_length() const { return max_read_length; }
+
+	int get_reads_count() const { return reads_count; }
+
+	long get_pg_length() const { return pg_length; }
 };
 
 #endif //PGRC_LEARN_PSEUDOGENOMEBASE_H
