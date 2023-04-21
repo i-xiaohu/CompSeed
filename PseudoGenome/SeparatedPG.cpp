80dfaa5778791ffeac244d90222817f4948f55be
i-xiaohu <631751599@qq.com>
2023-04-21 20:07:25 +0800

    SeparatedPG.cpp: get next read.

--- PseudoGenome/SeparatedPG.cpp
+++ PseudoGenome/SeparatedPG.cpp
@@ -2,6 +2,7 @@
 // Created by ixiaohu on 2023/4/19.
 //
 
+#include <cstring>
 #include "SeparatedPG.h"
 #include "../Utils/LzmaLib.h"
 
@@ -19,6 +20,27 @@ SeparatedPseudoGenome::SeparatedPseudoGenome(
 SeparatedPseudoGenome::~SeparatedPseudoGenome() {
 	delete reads_list;
 	delete properties;
+}
+
+void SeparatedPseudoGenome::get_next_mis_read(char *ptr) {
+	cur_pos += reads_list->off[next_idx];
+	memcpy(ptr, (pg_sequence.data() + cur_pos), reads_list->read_length);
+	if (reads_list->rev_comp[next_idx]) {
+		rev_comp_in_place(ptr, reads_list->read_length);
+	}
+	uint8_t mis_cnt = reads_list->mis_cnt[next_idx];
+	for (uint8_t i = 0; i < mis_cnt; i++) {
+		uint8_t mis_pos = reads_list->mis_off[cur_mis_cnt];
+		ptr[mis_pos] = code_to_mismatch(ptr[mis_pos], reads_list->mis_sym_code[cur_mis_cnt]);
+		cur_mis_cnt++;
+	}
+	next_idx++;
+}
+
+void SeparatedPseudoGenome::get_next_raw_read(char *ptr) {
+	cur_pos += reads_list->off[next_idx];
+	memcpy(ptr, (pg_sequence.data() + cur_pos), reads_list->read_length);
+	next_idx++;
 }
 
 void spg_decompress_reads_order(
--- PseudoGenome/SeparatedPG.h
+++ PseudoGenome/SeparatedPG.h
@@ -16,10 +16,21 @@ private:
 	ExtendReadsList *reads_list = nullptr;
 	ReadsSetProperties *properties = nullptr;
 
+	// Iteration variables
+	uint64_t cur_pos = 0;
+	uint64_t next_idx = 0;
+	uint64_t cur_mis_cnt = 0;
+
 public:
 	SeparatedPseudoGenome(std::string &&pg_seq, ExtendReadsList *rl, ReadsSetProperties *prop);
 
 	~SeparatedPseudoGenome();
+
+	/** Fetch next read with mismatches: reads in PG_hq */
+	void get_next_mis_read(char *ptr);
+
+	/** Fetch next read without mismatch: reads in PG_lq and PG_n */
+	void get_next_raw_read(char *ptr);
 };
 
 void spg_decompress_reads_order(
