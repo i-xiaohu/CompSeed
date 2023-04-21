6b28454626175d7dfd100c0b43c21f4bf1594733
i-xiaohu <631751599@qq.com>
2023-04-21 11:10:01 +0800

    manager.cpp: all PGs are decompressed.

--- manager.cpp
+++ manager.cpp
@@ -4,13 +4,13 @@
 
 #include "manager.h"
 
-#include <fstream>
 #include <cstring>
 #include <cassert>
 
 #include "PseudoGenome/PseudoGenomeBase.h"
 #include "PseudoGenome/ReadList/ExtendReadsList.h"
-#include "ReadsSet/ReadsSetBase.h"
+#include "PseudoGenome/SeparatedPG.h"
+#include "Matching/SimplePgMatcher.h"
 #include "Utils/helper.h"
 
 static const char *const GOOD_INFIX = "good";
@@ -20,7 +20,7 @@ static const char *const N_INFIX = "N";
 void Manager::load_all_PGs(ifstream &in) {
 	fprintf(stderr, "1. Decompressing HQ reads...\n");
 	PseudoGenomeHeader hq_pgh(in);
-	ReadsSetProperties hq_prop(in);
+	auto *hq_prop = new ReadsSetProperties(in);
 	assert(!confirm_text_read_mode(in));
 	assert(preserve_order_mode == false);
 	auto *hq_reads = load_extend_reads_list(
@@ -33,7 +33,7 @@ void Manager::load_all_PGs(ifstream &in) {
 
 	fprintf(stderr, "2. Decompressing LQ reads...\n");
 	PseudoGenomeHeader lq_pgh(in);
-	ReadsSetProperties lq_prop(in);
+	auto *lq_prop = new ReadsSetProperties(in);
 	assert(!confirm_text_read_mode(in));
 	auto *lq_reads = load_extend_reads_list(
 			in,
@@ -45,11 +45,11 @@ void Manager::load_all_PGs(ifstream &in) {
 
 	fprintf(stderr, "3. Decompressing N reads...\n");
 	PseudoGenomeHeader n_pgh;
-	ReadsSetProperties n_prop;
+	ReadsSetProperties *n_prop = nullptr;
 	ExtendReadsList *n_reads = nullptr;
 	if (separate_N) {
 		n_pgh = PseudoGenomeHeader(in);
-		n_prop = ReadsSetProperties(in);
+		n_prop = new ReadsSetProperties(in);
 		assert(!confirm_text_read_mode(in));
 		n_reads = load_extend_reads_list(
 				in,
@@ -59,6 +59,37 @@ void Manager::load_all_PGs(ifstream &in) {
 				true,
 				true);
 	}
+
+	read_length = hq_prop->max_read_length;
+	hq_reads_count = hq_prop->reads_count;
+	lq_reads_count = lq_prop->reads_count;
+	non_reads_count = hq_reads_count + lq_reads_count;
+	n_reads_count = separate_N ?n_prop->reads_count :0;
+	total_reads_count = non_reads_count + n_reads_count;
+
+	hq_pg_length = hq_pgh.get_pg_length();
+	non_pg_length = hq_pg_length + lq_pgh.get_pg_length();
+	if (preserve_order_mode) {
+		// wait to implement
+	} else {
+		spg_decompress_reads_order(
+				in,
+				idx_order,
+				preserve_order_mode,
+				take_pe_as_se,
+				single_end_mode);
+	}
+
+	string hq_pg_seq, lq_pg_seq, n_pg_seq;
+	restore_all_matched_pg(in, hq_pg_length, hq_pg_seq, lq_pg_seq, n_pg_seq);
+//	std::ofstream os("./pg.txt");
+//	os << hq_pg_seq << endl;
+//	os << lq_pg_seq << endl;
+//	os << n_pg_seq << endl;
+//	os.close();
+	hq_pg = new SeparatedPseudoGenome(std::move(hq_pg_seq), hq_reads, hq_prop);
+	lq_pg = new SeparatedPseudoGenome(std::move(lq_pg_seq), lq_reads, lq_prop);
+	n_pg = new SeparatedPseudoGenome(std::move(n_pg_seq), n_reads, n_prop);
 }
 
 void Manager::decompress() {
@@ -91,6 +122,7 @@ void Manager::decompress() {
     pg_N_prefix = tmp_directory + N_INFIX;
 
 	preserve_order_mode = (pgrc_mode == PGRC_ORD_SE_MODE or pgrc_mode == PGRC_ORD_PE_MODE);
+	take_pe_as_se = (pgrc_mode == PGRC_MIN_PE_MODE);
 	single_end_mode = (pgrc_mode == PGRC_SE_MODE or pgrc_mode == PGRC_ORD_SE_MODE);
 
 	load_all_PGs(in);
--- manager.h
+++ manager.h
@@ -6,13 +6,15 @@
 #define PGRC_LEARN_MANAGER_H
 
 #include <string>
+#include <vector>
+#include "PseudoGenome/SeparatedPG.h"
 using namespace std;
 
 static const char PGRC_SE_MODE = 0; // reordered Singled End
 static const char PGRC_PE_MODE = 1; // reordered Paired End
 static const char PGRC_ORD_SE_MODE = 2; // preserve Order Singled End
 static const char PGRC_ORD_PE_MODE = 3; // preserve Order Paired End
-static const char PGRC_MIN_PE_MODE = 4; // Compatible Paired End
+static const char PGRC_MIN_PE_MODE = 4; // Taking PE as SE
 
 static const char *const PGRC_HEADER = "PgRC";
 static const char PGRC_VERSION_MAJOR = 1;
@@ -26,8 +28,9 @@ private:
 	string archive_name;
 	uint8_t compression_level = 2;
 	bool separate_N = true; // separate reads with N
-	bool keep_pairing = false; // Keep pairing information
+	bool keep_pairing = true; // Keep pairing information
 	bool preserve_order_mode = false;
+	bool take_pe_as_se = false;
 	bool single_end_mode = false;
 
 	string pg_mapped_HQ_prefix;
@@ -35,6 +38,21 @@ private:
 	string pg_seq_final_HQ_prefix;
 	string pg_seq_final_LQ_prefix;
 	string pg_N_prefix;
+
+	int read_length;
+	int hq_reads_count;
+	int lq_reads_count;
+	int non_reads_count; // Non-N reads count
+	int n_reads_count;
+	int total_reads_count;
+
+	long hq_pg_length;
+	long non_pg_length; // Non-N PG length
+	SeparatedPseudoGenome *hq_pg = nullptr;
+	SeparatedPseudoGenome *lq_pg = nullptr;
+	SeparatedPseudoGenome *n_pg = nullptr;
+
+	vector<int> idx_order;
 public:
 	void set_archive_name(const char *fn) { this->archive_name = fn; }
 
