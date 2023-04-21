0c20389fa0d1bee369265f6cbb5d0847b9199d58
i-xiaohu <631751599@qq.com>
2023-04-21 11:08:39 +0800

    class SeparatedPseudoGenome added but not extend from base class.

--- PseudoGenome/SeparatedPG.cpp
+++ PseudoGenome/SeparatedPG.cpp
@@ -5,6 +5,22 @@
 #include "SeparatedPG.h"
 #include "../Utils/LzmaLib.h"
 
+SeparatedPseudoGenome::SeparatedPseudoGenome(
+		std::string &&pg_seq, ExtendReadsList *rl,ReadsSetProperties *prop) {
+	this->length = pg_seq.length();
+	this->properties = prop;
+	this->pg_sequence = std::move(pg_seq);
+	this->reads_list = rl;
+	if (this->reads_list) {
+		this->reads_list->reads_count = prop->reads_count;
+	}
+}
+
+SeparatedPseudoGenome::~SeparatedPseudoGenome() {
+	delete reads_list;
+	delete properties;
+}
+
 void spg_decompress_reads_order(
 		std::istream &in,
 		std::vector<int> idx_order,
--- PseudoGenome/SeparatedPG.h
+++ PseudoGenome/SeparatedPG.h
@@ -7,6 +7,20 @@
 
 #include <iostream>
 #include <vector>
+#include "ReadList/ExtendReadsList.h"
+
+class SeparatedPseudoGenome {
+private:
+	long length;
+	std::string pg_sequence;
+	ExtendReadsList *reads_list = nullptr;
+	ReadsSetProperties *properties = nullptr;
+
+public:
+	SeparatedPseudoGenome(std::string &&pg_seq, ExtendReadsList *rl, ReadsSetProperties *prop);
+
+	~SeparatedPseudoGenome();
+};
 
 void spg_decompress_reads_order(
 		std::istream &in,
