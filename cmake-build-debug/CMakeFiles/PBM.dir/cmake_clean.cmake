a46362c5a642d9dfab4e04a959564b2ab454748d
i-xiaohu <631751599@qq.com>
2023-06-22 15:27:37 +0800

    clean the project for testing seeding.

---
+++ cmake-build-debug/CMakeFiles/PBM.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PBM.dir/FM_index/QSufSort.c.o"
+  "CMakeFiles/PBM.dir/FM_index/bntseq.c.o"
+  "CMakeFiles/PBM.dir/FM_index/bwt.c.o"
+  "CMakeFiles/PBM.dir/FM_index/bwt_gen.c.o"
+  "CMakeFiles/PBM.dir/FM_index/is.c.o"
+  "CMakeFiles/PBM.dir/FM_index/rle.c.o"
+  "CMakeFiles/PBM.dir/FM_index/rope.c.o"
+  "CMakeFiles/PBM.dir/PgRC_BWA_MEM/bwamem.c.o"
+  "CMakeFiles/PBM.dir/PgRC_BWA_MEM/bwamem_extra.c.o"
+  "CMakeFiles/PBM.dir/PgRC_BWA_MEM/bwamem_pair.c.o"
+  "CMakeFiles/PBM.dir/PgRC_BWA_MEM/comp_seeding.cpp.o"
+  "CMakeFiles/PBM.dir/bwalib/bwa.c.o"
+  "CMakeFiles/PBM.dir/bwalib/bwashm.c.o"
+  "CMakeFiles/PBM.dir/bwalib/kopen.c.o"
+  "CMakeFiles/PBM.dir/bwalib/ksw.c.o"
+  "CMakeFiles/PBM.dir/bwalib/utils.c.o"
+  "CMakeFiles/PBM.dir/cstl/kstring.c.o"
+  "CMakeFiles/PBM.dir/cstl/kthread.c.o"
+  "PBM.exe"
+  "PBM.pdb"
+  "libPBM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/PBM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/comp_seed.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/QSufSort.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/QSufSort.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/bntseq.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/bntseq.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/bwt.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/bwt.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/bwt_gen.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/bwt_gen.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/is.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/is.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/rle.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/rle.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/FM_index/rope.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/FM_index/rope.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/bwalib/bwa.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/bwalib/bwa.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/bwalib/bwashm.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/bwalib/bwashm.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/bwalib/kopen.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/bwalib/kopen.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/bwalib/ksw.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/bwalib/ksw.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/bwalib/utils.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/bwalib/utils.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/cstl/kstring.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/cstl/kstring.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/cstl/kthread.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/cstl/kthread.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/seeding/bwamem.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/seeding/bwamem.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/seeding/bwamem_extra.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/seeding/bwamem_extra.c.o"
+  "/cygdrive/d/ClionProject/comp_seed/seeding/bwamem_pair.c" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/seeding/bwamem_pair.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/d/ClionProject/comp_seed/seeding/comp_seeding.cpp" "/cygdrive/d/ClionProject/comp_seed/cmake-build-debug/CMakeFiles/comp_seed.dir/seeding/comp_seeding.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
