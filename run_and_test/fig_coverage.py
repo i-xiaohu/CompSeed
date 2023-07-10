81609b2494bec79c385ff70924ad2a6be7289434
i-xiaohu <631751599@qq.com>
2023-07-10 14:28:23 +0800

    fig_coverage.py: figure in 1 x 3 layout.

--- run_and_test/fig_coverage.py
+++ run_and_test/fig_coverage.py
@@ -27,7 +27,7 @@ def run():
     pgrc =     [23492, 36725, 45163, 49366,  59632]
 
     plt.figure()
-    ax = plt.subplot(221)
+    ax = plt.subplot(131)
     ax.bar([x + 0 for x in base_x], bwa_mem, align='edge', width=1.5, color=light_red, label='BWA-MEM')
     ax.bar([x + 2.5 for x in base_x], spring,  align='edge', width=1.5, color=light_blue, label='SPRING')
     ax.bar([x + 4.5 for x in base_x], minicom, align='edge', width=1.5, color=light_green, label='Minicom')
@@ -43,34 +43,34 @@ def run():
     ax.legend(prop=font1)
 
     # Figure B
-    bwa_mem = [22580690796, 44389394657, 65878092845, 87258049888, 108613279707]
-    bwa_mem = [b / 1000000000 for b in bwa_mem]
-    spring =  [1046353920, 1269032960, 1500477440, 1724917760, 1939814400]
-    spring =  [s / 1000000000 for s in spring]
-    minicom = [1533306880, 1840066560, 2106398720, 2356889600, 2591240960]
-    minicom = [m / 1000000000 for m in minicom]
-    pgrc =    [1125829786, 1206283685, 1358332405, 1518122050, 1672546991]
-    pgrc =    [p / 1000000000 for p in pgrc]
-    ax = plt.subplot(222)
-    ax.bar([x + 0 for x in base_x], bwa_mem, align='edge', width=1.5, color=light_red, label='BWA-MEM')
-    ax.bar([x + 2.5 for x in base_x], spring,  align='edge', width=1.5, color=light_blue, label='SPRING')
-    ax.bar([x + 4.5 for x in base_x], minicom, align='edge', width=1.5, color=light_green, label='Minicom')
-    ax.bar([x + 6.5 for x in base_x], pgrc,    align='edge', width=1.5, color=light_yellow, label='PgRC')
-    ax.set_xlabel("Coverage", fontdict=font2)
-    ax.set_ylabel("Size(GB)", fontdict=font2)
-    ax.set_xticks([x + 3.5 for x in base_x])
-    ax.set_xticklabels(coverage)
-    ax.tick_params(axis='x', length=0)
-    ax.tick_params(labelsize=15)
-    tick_labels = ax.get_xticklabels() + ax.get_yticklabels()
-    [tl.set_fontname('Times New Roman') for tl in tick_labels]
+    # bwa_mem = [22580690796, 44389394657, 65878092845, 87258049888, 108613279707]
+    # bwa_mem = [b / 1000000000 for b in bwa_mem]
+    # spring =  [1046353920, 1269032960, 1500477440, 1724917760, 1939814400]
+    # spring =  [s / 1000000000 for s in spring]
+    # minicom = [1533306880, 1840066560, 2106398720, 2356889600, 2591240960]
+    # minicom = [m / 1000000000 for m in minicom]
+    # pgrc =    [1125829786, 1206283685, 1358332405, 1518122050, 1672546991]
+    # pgrc =    [p / 1000000000 for p in pgrc]
+    # ax = plt.subplot(222)
+    # ax.bar([x + 0 for x in base_x], bwa_mem, align='edge', width=1.5, color=light_red, label='BWA-MEM')
+    # ax.bar([x + 2.5 for x in base_x], spring,  align='edge', width=1.5, color=light_blue, label='SPRING')
+    # ax.bar([x + 4.5 for x in base_x], minicom, align='edge', width=1.5, color=light_green, label='Minicom')
+    # ax.bar([x + 6.5 for x in base_x], pgrc,    align='edge', width=1.5, color=light_yellow, label='PgRC')
+    # ax.set_xlabel("Coverage", fontdict=font2)
+    # ax.set_ylabel("Size(GB)", fontdict=font2)
+    # ax.set_xticks([x + 3.5 for x in base_x])
+    # ax.set_xticklabels(coverage)
+    # ax.tick_params(axis='x', length=0)
+    # ax.tick_params(labelsize=15)
+    # tick_labels = ax.get_xticklabels() + ax.get_yticklabels()
+    # [tl.set_fontname('Times New Roman') for tl in tick_labels]
 
     # Figure c
     bwa_mem = [624, 623, 622, 622, 622]
     spring =  [335, 287, 256, 233, 216]
     minicom = [339, 298, 270, 248, 218]
     pgrc =    [346, 292, 257, 233, 214]
-    ax = plt.subplot(223)
+    ax = plt.subplot(132)
     ax.bar([x + 0 for x in base_x], bwa_mem, align='edge', width=1.5, color=light_red, label='BWA-MEM')
     ax.bar([x + 2.5 for x in base_x], spring,  align='edge', width=1.5, color=light_blue, label='SPRING')
     ax.bar([x + 4.5 for x in base_x], minicom, align='edge', width=1.5, color=light_green, label='Minicom')
@@ -89,7 +89,7 @@ def run():
     spring =  [15.22, 12.96, 11.54, 10.54, 9.80]
     minicom = [15.81, 13.75, 12.43, 11.48, 10.72]
     pgrc =    [15.78, 13.28, 11.76, 10.72, 9.93]
-    ax = plt.subplot(224)
+    ax = plt.subplot(133)
     ax.bar([x + 0 for x in base_x], bwa_mem, align='edge', width=1.5, color=light_red, label='BWA-MEM')
     ax.bar([x + 2.5 for x in base_x], spring,  align='edge', width=1.5, color=light_blue, label='SPRING')
     ax.bar([x + 4.5 for x in base_x], minicom, align='edge', width=1.5, color=light_green, label='Minicom')
