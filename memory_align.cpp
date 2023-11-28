e26985260bedf541357c32d2a1b44e90fdaca1ae
i-xiaohu <631751599@qq.com>
2023-11-29 00:23:10 +0800

    memory_align.cpp: analyze four ways of profiling program runtime.

--- memory_align.cpp
+++ memory_align.cpp
@@ -6,129 +6,80 @@
 #include <cstdlib>
 #include <chrono>
 #include <cstdio>
+#include <unistd.h>
+#include <sys/time.h>
+#include <x86intrin.h>
 
-#include "mm_malloc.h"
 using namespace std::chrono;
 
-
-void granularity_1(uint32_t n, void *a) {
-	auto data8 = (uint8_t *)a;
-	auto dataEnd = data8 + n;
-	while (data8 != dataEnd) {
-		*data8 = ~(*data8);
-		data8++;
-	}
+static inline long gettimeofday_wrapper() {
+	struct timeval tp;
+	gettimeofday(&tp, nullptr);
+	return tp.tv_sec  * 1000000 + tp.tv_usec;
 }
 
-void granularity_2(uint32_t n, void *a) {
-	auto data16 = (uint16_t *)a;
-	auto data16End = data16 + (n >> 1U);
-	auto data8 = (uint8_t*)data16End;
-	auto data8End = data8 + (n & 1U);
-	while (data16 != data16End) {
-		*data16 = ~(*data16);
-		data16++;
-	}
-	while (data8 != data8End) {
-		*data8 = ~(*data8);
-		data8++;
-	}
-}
-
-void granularity_q(uint32_t n, void *a) {
-	auto data32 = (uint32_t *)a;
-	auto data32End = data32 + (n >> 2U);
-	auto data8 = (uint8_t*)data32End;
-	auto data8End = data8 + (n & 3U);
-	while (data32 != data32End) {
-		*data32 = ~(*data32);
-		data32++;
-	}
-	while (data8 != data8End) {
-		*data8 = ~(*data8);
-		data8++;
-	}
-}
-
-void granularity_8(uint32_t n, void *a) {
-	auto data64 = (uint64_t *)a;
-	auto data64End = data64 + (n >> 3U);
-	auto data8 = (uint8_t*)data64End;
-	auto data8End = data8 + (n & 7U);
-	while (data64 != data64End) {
-		*data64 = ~(*data64);
-		data64++;
-	}
-	while (data8 != data8End) {
-		*data8 = ~(*data8);
-		data8++;
-	}
+static inline long clock_gettime_wrapper() {
+	struct timespec tp;
+	clock_gettime(CLOCK_MONOTONIC, &tp);
+	return tp.tv_sec * 1000000000 + tp.tv_nsec;
 }
 
 int main() {
-	void *a;
-	uint32_t n = 800 * 1024 * 1024;
+	long n = 1e8;
 	time_point<system_clock, nanoseconds> t_start, t_end;
 	nanoseconds elapsed;
 
-	// Unaligned memory block
-	a = (uint8_t*) malloc((n + 1) * sizeof(uint8_t));
-	a = (uint8_t*)a + 1; // 1 byte bias
-	fprintf(stderr, "Pointer %% 8 = %lu\n", (uint64_t)a % 8);
-	// Memory granularity: 1 byte
+	// std::chrono
 	t_start = std::chrono::high_resolution_clock::now();
-	granularity_1(n, a);
+	long num = 0; // For validate
+	for (long i = 0; i < n; i++) { // Run time of empty loop is 266ns
+		auto x = std::chrono::high_resolution_clock::now();
+		auto y = std::chrono::high_resolution_clock::now();
+		num += (y - x).count();
+	}
 	t_end = std::chrono::high_resolution_clock::now();
 	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 1: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	// Memory granularity: 2 byte
-	t_start = std::chrono::high_resolution_clock::now();
-	granularity_2(n, a);
-	t_end = std::chrono::high_resolution_clock::now();
-	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 2: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	// Memory granularity: 4 byte
-	t_start = std::chrono::high_resolution_clock::now();
-	granularity_q(n, a);
-	t_end = std::chrono::high_resolution_clock::now();
-	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 4: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	// Memory granularity: 8 byte
-	t_start = std::chrono::high_resolution_clock::now();
-	granularity_8(n, a);
-	t_end = std::chrono::high_resolution_clock::now();
-	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 8: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	free((uint8_t*)a - 1);
+	fprintf(stderr, "Overhead of std::chrono %.2f ns (verify=%.2f ns)\n", (double)elapsed.count() / n / 2, (double)num / n);
 
-	// Aligned memory block
-	a = (uint8_t*) _mm_malloc(n * sizeof(uint8_t), 64);
-	fprintf(stderr, "Pointer %% 8 = %lu\n", (uint64_t)a % 8);
-	// Memory granularity: 1 byte
+	// gettimeofday
 	t_start = std::chrono::high_resolution_clock::now();
-	granularity_1(n, a);
+	num = 0; // For validate
+	for (long i = 0; i < n; i++) {
+		auto x = gettimeofday_wrapper();
+		auto y = gettimeofday_wrapper();
+		num += y - x;
+	}
 	t_end = std::chrono::high_resolution_clock::now();
 	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 1: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	// Memory granularity: 2 byte
+	fprintf(stderr, "Overhead of gettimeofday %.2f ns (verify=%.2f ns)\n", (double)elapsed.count() / n / 2, (double)num / n);
+
+	// clock_gettime
 	t_start = std::chrono::high_resolution_clock::now();
-	granularity_2(n, a);
+	num = 0; // For validate
+	for (long i = 0; i < n; i++) {
+		auto x = clock_gettime_wrapper();
+		auto y = clock_gettime_wrapper();
+		num += y - x;
+	}
 	t_end = std::chrono::high_resolution_clock::now();
 	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 2: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	// Memory granularity: 4 byte
+	fprintf(stderr, "Overhead of clock_gettime %.2f ns (verify=%.2f ns)\n", (double)elapsed.count() / n / 2, (double)num / n);
+
+	// __rdtsc
+	uint64_t tim = __rdtsc();
+	sleep(1);
+	uint64_t proc_freq = __rdtsc() - tim;
+	fprintf(stderr, "CPU frequency: %ld\n", proc_freq);
 	t_start = std::chrono::high_resolution_clock::now();
-	granularity_q(n, a);
+	num = 0; // For validate
+	for (long i = 0; i < n; i++) {
+		uint64_t x = __rdtsc();
+		uint64_t y = __rdtsc();
+		num += y - x;
+	}
 	t_end = std::chrono::high_resolution_clock::now();
 	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 4: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	// Memory granularity: 8 byte
-	t_start = std::chrono::high_resolution_clock::now();
-	granularity_8(n, a);
-	t_end = std::chrono::high_resolution_clock::now();
-	elapsed = t_end - t_start;
-	fprintf(stderr, "Granularity 8: %.0f milliseconds\n", elapsed.count() / 1000.0);
-	_mm_free(a);
+	fprintf(stderr, "Overhead of __rdtsc %.2f ns (verify=%.2f cycles or %.2f ns)\n", (double)elapsed.count() / n / 2, (double)num / n, (double)num / n / proc_freq * 1e9);
 
 	return 0;
 }
